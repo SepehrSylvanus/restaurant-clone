38de0c8e5a7bf1304b83ebc8f7f778ed8ba94c30
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2023-11-17 15:15:55 +0330

    main page front

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import styles from './navbar.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 const Navbar = () => {
+  const [activeLink, setActive] = useState(null)
+  const handleLink = e=>{
+    setActive(e.target.pathanme)
+  }
   return (
     <div className={styles.container}>
-      <Link>About</Link>
-      <Link>Cuisine</Link>
-      <Link>Menus</Link>
-      <Link>Reservations</Link>
-      <Link>Private dining</Link>
+      <div className={styles.imgContainer}>
+        <Image src={'/kokkariLogo.png'} alt='' fill/>
+      </div>
+    <div className={styles.navLinks}>
+    <Link href={'/about'}>About</Link>
+      <Link href={'/cuisine'}  onClick={handleLink}>Cuisine</Link>
+      <Link href={'/menus'}  onClick={handleLink}>Menus</Link>
+      <Link href={'/reservations'}  onClick={handleLink}>Reservations</Link>
+      <Link href={'/private-dining'}  onClick={handleLink}>Private dining</Link>
+    </div>
     </div>
   )
 }
