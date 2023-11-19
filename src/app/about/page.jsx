43df769d0df879071869b1642ad71c66fc675b73
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2023-11-19 18:22:34 +0330

    about page front

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,17 +3,19 @@ import Navbar from "../components/navbar/Navbar";
 import styles from "./about.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import './aboutGlobal.css'
+import "./aboutGlobal.css";
 const AboutPage = () => {
   return (
     <div className={styles.container}>
       <Navbar />
       <div className={styles.mainContainer}>
         <h1>Philoxenia — φιλοξενια</h1>
+        <hr className={styles.responsiveDivider} />
+        <hr className={styles.responsiveDivider} style={{marginBottom: '1em'}}/>
         <div className={styles.aboutHotel}>
           <div className={styles.textContainer}>
-            <hr />
-            <hr />
+            <hr className={styles.bigDivider}/>
+            <hr className={styles.bigDivider}/>
 
             <p>
               At Kokkari Estiatorio, we are driven by the concept of philoxenia,
@@ -54,32 +56,86 @@ const AboutPage = () => {
         <div className={styles.aboutSeo}>
           <div className={styles.hotelNamesAndAddress}>
             <div className={styles.address}>
-              <h3 style={{marginBottom: '1em'}}>KOKKARI ESTIATORIO</h3>
-              <p style={{marginBottom: '1em', fontWeight: 'bold'}}>
+              <h3 style={{ marginBottom: "1em" }}>KOKKARI ESTIATORIO</h3>
+              <p style={{ marginBottom: "1em", fontWeight: "bold" }}>
                 200 Jackson Street (at Front St.)
                 <br />
                 San Francisco, CA 94111
               </p>
               <p>
-                p: <Link href={'tel:+14159810983'} style={{fontFamily: 'Arial, Helvetica, sans-serif', fontSize: 16, fontWeight: 'bold'}} className={styles.roleLinks}>415.981.0983</Link>
+                p:{" "}
+                <Link
+                  href={"tel:+14159810983"}
+                  style={{
+                    fontFamily: "Arial, Helvetica, sans-serif",
+                    fontSize: 16,
+                    fontWeight: "bold",
+                  }}
+                  className={styles.roleLinks}
+                >
+                  415.981.0983
+                </Link>
                 <br />
               </p>
-              <Link href={'/reservations'} style={{fontFamily: 'Arial, Helvetica, sans-serif', fontSize: 16, fontWeight: 'bold'}} className={styles.roleLinks}>reservations & hours</Link>
-              <Link href={'http://maps.apple.com/?daddr=200+Jackson+Street,+ San+Francisco,+CA'} style={{fontFamily: 'Arial, Helvetica, sans-serif', fontSize: 16, fontWeight: 'bold', display: 'block'}} className={styles.roleLinks}>directions via mobile map</Link>
+              <Link
+                href={"/reservations"}
+                style={{
+                  fontFamily: "Arial, Helvetica, sans-serif",
+                  fontSize: 16,
+                  fontWeight: "bold",
+                }}
+                className={styles.roleLinks}
+              >
+                reservations & hours
+              </Link>
+              <Link
+                href={
+                  "http://maps.apple.com/?daddr=200+Jackson+Street,+ San+Francisco,+CA"
+                }
+                style={{
+                  fontFamily: "Arial, Helvetica, sans-serif",
+                  fontSize: 16,
+                  fontWeight: "bold",
+                  display: "block",
+                }}
+                className={styles.roleLinks}
+              >
+                directions via mobile map
+              </Link>
             </div>
             <div className={styles.roles}>
               <p>Managing Partner</p>
-              <Link href={'mailto:paul@kokkari.com'} className={styles.rolaNameLinks}>Paul Kirby</Link>
+              <Link
+                href={"mailto:paul@kokkari.com"}
+                className={styles.rolaNameLinks}
+              >
+                Paul Kirby
+              </Link>
               <p>Chef Partner </p>
-              <Link href={'mailto:erik@kokkari.com'} className={styles.rolaNameLinks}>Erik Cosselmon</Link>
+              <Link
+                href={"mailto:erik@kokkari.com"}
+                className={styles.rolaNameLinks}
+              >
+                Erik Cosselmon
+              </Link>
               <p>General Manager </p>
-              <Link href={'mailto:william@kokkari.com'} className={styles.rolaNameLinks}>William Boumier</Link>
+              <Link
+                href={"mailto:william@kokkari.com"}
+                className={styles.rolaNameLinks}
+              >
+                William Boumier
+              </Link>
               <p>Executive Chef </p>
               <p>Mario Ortega</p>
               <p>Chef de Cuisine </p>
               <p>Manuel Vera</p>
               <p>Office Manager </p>
-              <p>Molly Barrango</p>
+              <Link
+                href={"mailto:molly@kokkari.com"}
+                className={styles.rolaNameLinks}
+              >
+                Molly Barrango
+              </Link>
               <p>Sous Chefs </p>
               <p> Miho Reis</p>
               <p>Pastry</p>
@@ -92,24 +148,53 @@ const AboutPage = () => {
                 <br />
                 Michele DiRuocco
                 <br />
-                Robin Kirby
+                <Link
+                  href={"mailto:robin@kokkari.com"}
+                  className={styles.rolaNameLinks}
+                >
+                  {" "}
+                  Robin Kirby
+                </Link>
                 <br />
                 Valerie Klein
               </p>
               <p>Wine Buyer </p>
               <p>Lyle Coffield</p>
               <p>Event Manager </p>
-              <p>Melissa Lopez</p>
+              <Link
+                href={"mailto:events@kokkari.com"}
+                className={styles.rolaNameLinks}
+              >
+                Melissa Lopez
+              </Link>
             </div>
           </div>
           <div className={styles.aboutEachOne}>
             <div className={styles.eachRole}>
               <div className={styles.paulImgContainer}>
-              <Image src={'/paul.jpg'} width={300} height={449} className={styles.img}/>
+                <Image
+                  src={"/paul.jpg"}
+                  width={300}
+                  height={449}
+                  className={styles.img}
+                />
               </div>
-              <Link href={'mailto:paul@kokkari.com'} className={styles.roleLinks}><h4 style={{marginBottom: '1em'}}>Paul kirby</h4></Link>
-              <h5 style={{fontFamily: 'Arial, Helvetica, sans-serif', fontSize: 16, fontWeight: 'normal'}}>Managing partner</h5>
-              <p style={{margin: '1em 0'}}>
+              <Link
+                href={"mailto:paul@kokkari.com"}
+                className={styles.roleLinks}
+              >
+                <h4 style={{ marginBottom: "1em" }}>Paul kirby</h4>
+              </Link>
+              <h5
+                style={{
+                  fontFamily: "Arial, Helvetica, sans-serif",
+                  fontSize: 16,
+                  fontWeight: "normal",
+                }}
+              >
+                Managing partner
+              </h5>
+              <p style={{ margin: "1em 0" }}>
                 “Running a restaurant is all about taking care of people,” says
                 Paul Kirby who has been at the helm of Kokkari Estiatorio since
                 2000. “From your staff to each and every guest that comes into
@@ -118,7 +203,7 @@ const AboutPage = () => {
                 and respected take pride in their work and truly enjoy their
                 jobs. This translates directly to the guest experience.”
               </p>
-              <p style={{margin: '1em 0'}}>
+              <p style={{ margin: "1em 0" }}>
                 While Paul may be soft-spoken and charming (qualities he
                 attributes to his Southern upbringing), he is extremely driven.
                 His dedicated, hands-on approach to management ensures the high
@@ -128,7 +213,7 @@ const AboutPage = () => {
                 of hospitality: Have a good time and above all, make sure the
                 guests have a great time.
               </p>
-              <p style={{margin: '1em 0'}}>
+              <p style={{ margin: "1em 0" }}>
                 Paul came to the West Coast after years managing waterfront
                 restaurant properties in The Hamptons outside of New York City.
                 His first job in San Francisco was working alongside acclaimed,
@@ -138,6 +223,126 @@ const AboutPage = () => {
                 Meditteranean cuisine and wines, an essential prerequisite for
                 working at Kokkari. In 2006 he was named Managing Partner of
                 both Kokkari and Evvia in Palo Alto.
+              </p>
+            </div>
+            <div className={styles.eachRole}>
+              <div className={styles.paulImgContainer}>
+                <Image
+                  src={"/erik.jpg"}
+                  width={300}
+                  height={449}
+                  className={styles.img}
+                />
+              </div>
+              <Link
+                href={"mailto:erik@kokkari.com"}
+                className={styles.roleLinks}
+              >
+                <h4 style={{ marginBottom: "1em" }}>Erik Cosselmon</h4>
+              </Link>
+              <h5
+                style={{
+                  fontFamily: "Arial, Helvetica, sans-serif",
+                  fontSize: 16,
+                  fontWeight: "normal",
+                }}
+              >
+                Chef Partner
+              </h5>
+              <p style={{ margin: "1em 0" }}>
+                Erik Cosselmon’s wide-ranging Mediterranean repertoire and
+                strong, ingredient-driven style finds full expression in the
+                Greek-inspired menus at Kokkari and sister restaurant Evvia in
+                Palo Alto. Since being named Executive Chef of Kokkari in 2004,
+                Cosselmon’s brand of casual tavern cuisine has attracted even
+                more devotees, earning Kokkari top ratings in both local and
+                National press publications. “The goal at Kokkari and Evvia is
+                to offer as close to a true Greek restaurant experience as
+                possible. Not only is it about good food, it’s about the people,
+                the celebratory atmosphere and the Greek hospitality.”
+              </p>
+              <p style={{ margin: "1em 0" }}>
+                Erik grew up on a farm in Central Michigan helping his mom tend
+                their extensive kitchen garden and his dad cook for the large
+                parties the family would host. This introduction to farm-fresh
+                ingredients and cooking cemented his desire to be a chef at an
+                early age. After completing a degree at The Culinary Institute
+                of America in Hyde Park, New York he moved to Manhattan and
+                began his career working at notable restaurants such as
+                Montrachet, Tribeca Grill, Le Bernardin and Daniel.
+              </p>
+              <p style={{ margin: "1em 0" }}>
+                Cosselmon moved to San Francisco and in 1996 joined Reed Hearon
+                in opening his landmark Liqurian restaurant, Rose Pistola, in
+                North Beach. In 1998 he was named Executive Chef. In 2001, ready
+                for a new challenge, Erik left to run the kitchen at Cetrella
+                Bistro & Café in Half Moon Bay where he garnered immediate and
+                consistent critical praise for his rustic Mediterranean food
+              </p>
+              <p style={{ margin: "1em 0" }}>
+                His subsequent move to Kokkari made perfect sense. “Since moving
+                from New York to the Bay Area, my cooking philosophy has evolved
+                from the formal French tradition to the less complicated
+                Mediterranean, with a focus on ingredients as opposed to the
+                architecture of the dish.” His leadership and cuisine has
+                contributed to Kokkari’s extraordinary success as one of San
+                Francisco’s busiest restaurants. In 2006, Cosselmon was named
+                Executive Chef of sister restaurant Evvia located in Palo Alto
+                where he oversees the menu. He also joined the founding Partners
+                as a Partner.
+              </p>
+            </div>
+            <div className={styles.eachRole}>
+              <div className={styles.paulImgContainer}>
+                <Image
+                  src={"/mario.jpg"}
+                  width={300}
+                  height={449}
+                  className={styles.img}
+                />
+              </div>
+              <div className={styles.roleLinks}>
+                <h4 style={{ marginBottom: "1em" }}>Mario Ortega</h4>
+              </div>
+              <h5
+                style={{
+                  fontFamily: "Arial, Helvetica, sans-serif",
+                  fontSize: 16,
+                  fontWeight: "normal",
+                }}
+              >
+                Executive Chef
+              </h5>
+              <p style={{ margin: "1em 0" }}>
+                Chef Mario Ortega’s interest in food and cooking began at a
+                young age. He would spend hours in the kitchen with his grandma
+                observing and helping her cook and tasting the delicious dishes
+                she created. This ignited a lifelong passion for food that
+                shaped his adult life.
+              </p>
+              <p style={{ margin: "1em 0" }}>
+                After attending culinary school at Leaderwolf Academy in
+                Sacramento, Chef Mario was brought to California’s culinary
+                mecca, San Francisco. During his time in SF, Mario worked hard
+                to develop his craft at high end restaurants such as Vertigo in
+                the famous TransAmerica building and Scala’s Bistro. He spent
+                several years at Scala’s nourishing his love for Mediterranean
+                cooking. In 2005 he left the city for an opportunity to head the
+                kitchen at Quail Lodge Resort in Carmel. He joined the culinary
+                team, and a few years later as as Chef de Cuisine at Evvia
+                Estiatorio, our sister restaurant in Palo Alto. The modern
+                Hellenic cuisine was a great fit for Mario’s
+                Mediterranean-focused background.
+              </p>
+              <p style={{ margin: "1em 0" }}>
+                After 15 years at Evvia Estiatorio, Mario returned to Kokkari in
+                the Summer of 2023 to assume the roll of Executive Chef. During
+                his time at Evvia he developed close relationships with many
+                local purveyors. At Kokkari, he continues to foster these
+                relationships and focus on creating Mediterranean-inspired
+                dishes from locally sourced products whenever possible. When
+                Chef is not at the helm at Evvia, he adores spending time with
+                his wife and three children and it usually involves food!
               </p>
             </div>
           </div>
--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -5,17 +5,11 @@ import Link from "next/link";
 import Image from "next/image";
 import MenuIcon from "@mui/icons-material/Menu";
 import MenuOpenIcon from "@mui/icons-material/MenuOpen";
-import { useSpring, animated } from '@react-spring/web'
 const Navbar = () => {
   const [path, setPath] = useState(null);
   const [menuOpen, setMenuOpen] = useState(false);
-  const menuAnimation = useSpring({
-    transform: menuOpen ? 'translate3d(0,0,0)' : 'translate3d(100%,0,0)',
-    config: {
-      tension: 210,
-      friction: 20,
-    },
-  });
+
+
   useEffect(() => {
     const changeLink = () => {
       const path = window.location.pathname;
@@ -92,60 +86,57 @@ const Navbar = () => {
           </div>
         )}
 
-
-<animated.div
-style={menuAnimation}>
-<div
-        className={styles.navLinksResponsive}
-        style={menuOpen ? { display: "flex" } : { display: "none" }}
-      >
-        <Link href={"/about"}>
-          <span
+        <div
+          className={styles.navLinksResponsive}
+          style={menuOpen ? { display: "flex" } : { display: "none" }}
+        >
+          <Link href={"/about"}>
+            <span
+              style={
+                path === "about" ? { color: "#6d4412" } : { color: "#333333" }
+              }
+            >
+              About
+            </span>
+          </Link>
+          <Link
+            href={"/cuisine"}
             style={
-              path === "about" ? { color: "#6d4412" } : { color: "#333333" }
+              path === "cuisine" ? { color: "#6d4412" } : { color: "#333333" }
             }
           >
-            About
-          </span>
-        </Link>
-        <Link
-          href={"/cuisine"}
-          style={
-            path === "cuisine" ? { color: "#6d4412" } : { color: "#333333" }
-          }
-        >
-          Cuisine
-        </Link>
-        <Link
-          href={"/menus"}
-          style={path === "menus" ? { color: "#6d4412" } : { color: "#333333" }}
-        >
-          Menus
-        </Link>
-        <Link
-          href={"/reservations"}
-          style={
-            path === "reservations"
-              ? { color: "#6d4412" }
-              : { color: "#333333" }
-          }
-        >
-          Reservations
-        </Link>
-        <Link
-          href={"/private-dining"}
-          style={
-            path === "private-dining"
-              ? { color: "#6d4412" }
-              : { color: "#333333" }
-          }
-        >
-          Private dining
-        </Link>
+            Cuisine
+          </Link>
+          <Link
+            href={"/menus"}
+            style={
+              path === "menus" ? { color: "#6d4412" } : { color: "#333333" }
+            }
+          >
+            Menus
+          </Link>
+          <Link
+            href={"/reservations"}
+            style={
+              path === "reservations"
+                ? { color: "#6d4412" }
+                : { color: "#333333" }
+            }
+          >
+            Reservations
+          </Link>
+          <Link
+            href={"/private-dining"}
+            style={
+              path === "private-dining"
+                ? { color: "#6d4412" }
+                : { color: "#333333" }
+            }
+          >
+            Private dining
+          </Link>
+        </div>
       </div>
-</animated.div>
-      </div>
-      
     </div>
   );
 };
