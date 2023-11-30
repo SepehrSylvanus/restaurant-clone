a1747c2f2fda1f44f71aea1273027f0554af08b8
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2023-11-30 10:53:01 +0330

    clean checkout form

--- src/app/cookbook/page.jsx
+++ src/app/cookbook/page.jsx
@@ -9,7 +9,7 @@ import Textarea from "../components/textarea/Textarea";
 import { Divider } from "@mui/material";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements, PaymentElement } from "@stripe/react-stripe-js";
-import CheckoutForm from "../components/CheckoutForm";
+
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
 );
